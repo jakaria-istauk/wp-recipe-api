4ba2006f17516746a45a7cf02ce681c746760e90
Jakaria Istauk <jakariamd35@gmail.com>
2023-09-05 23:43:50 +0600

    creating recipe .....

--- Apps/Api.php
+++ Apps/Api.php
@@ -10,102 +10,39 @@ class Api
 	    $plugin_dir_path = plugin_dir_path(__FILE__);
 	    require_once( $plugin_dir_path . 'Helper.php');
     }
-    public function init() {
+
+	public function init() {
 
 		$api_name_space = 'recipe-api/v1';
-        register_rest_route($api_name_space, '/recipes', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'get_all_recipes'),
-        ));
 
-        register_rest_route($api_name_space, '/recipe/(?P<slug>[a-zA-Z0-9-]+)', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'get_recipe_by_slug'),
-        ));
+		register_rest_route( $api_name_space, '/user/signup', array(
+			'methods'  => 'POST',
+			'callback' => array( $this, 'register_user' ),
+		) );
 
-	    register_rest_route($api_name_space, '/recipe', array(
-		    'methods' => 'POST',
-		    'callback' => array($this, 'create_a_recipe'),
-	    ));
+		register_rest_route( $api_name_space, '/user/login', array(
+			'methods'  => 'POST',
+			'callback' => array( $this, 'user_validation' ),
+		) );
+		register_rest_route( $api_name_space, '/recipes', array(
+			'methods'  => 'GET',
+			'callback' => array( $this, 'get_all_recipes' ),
+		) );
 
-		register_rest_route($api_name_space, '/recipes/(?P<id>\d+)', array(
-            'methods' => 'GET, POST, PUT, DELETE',
-            'callback' => array($this, 'recipe_api_handler'),
-        ));
+		register_rest_route( $api_name_space, '/recipe/(?P<slug>[a-zA-Z0-9-]+)', array(
+			'methods'  => 'GET',
+			'callback' => array( $this, 'get_recipe_by_slug' ),
+		) );
 
-	    register_rest_route($api_name_space, '/user/signup', array(
-		    'methods' => 'POST',
-		    'callback' => array($this, 'register_user'),
-	    ));
+		register_rest_route( $api_name_space, '/recipe', array(
+			'methods'  => 'POST',
+			'callback' => array( $this, 'create_a_recipe' ),
+		) );
 
-	    register_rest_route($api_name_space, '/user/login', array(
-		    'methods' => 'POST',
-		    'callback' => array($this, 'user_validation'),
-	    ));
-    }
-
-    // Callback function to get all recipes
-	public function get_all_recipes( $request ) {
-		$args = array(
-			'post_type' => 'recipe',
-			'per_page'  => - 1, // Retrieve all posts of the 'recipe' type
-		);
-
-		$query   = new \WP_Query( $args );
-		$recipes = [];
-
-		if ( $query->have_posts() ) {
-			$posts = $query->posts;
-
-			foreach ( $posts as $post ) {
-				$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-				$recipes[] = [
-					'id'          => $post->ID,
-					'title'       => $post->post_title,
-					'description' => $post->post_content,
-					'slug'        => $post->post_name,
-					'image'       => $thumbnail ? $thumbnail[0] : $thumbnail,
-					'ingredients' => get_post_meta( $post->ID, '_recipe_ingredients', true ),
-					'user_hash'   => Helper::encrypt( "{$post->post_author}author" )
-				];
-			}
-		}
-
-		return rest_ensure_response( $recipes );
-	}
-
-	public function get_recipe_by_slug( $request ) {
-
-		$recipe = get_page_by_path( $request->get_param( 'slug' ), OBJECT, 'recipe' );
-		if ( $recipe ) {
-			$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $recipe->ID ), 'full' );
-			$response  = [
-				'id'          => $recipe->ID,
-				'title'       => $recipe->post_title,
-				'description' => $recipe->post_content,
-				'slug'        => $recipe->post_name,
-				'image'       => $thumbnail ? $thumbnail[0] : $thumbnail,
-				'ingredients' => get_post_meta( $recipe->ID, '_recipe_ingredients', true ),
-				'user_hash'   => Helper::encrypt( "{$recipe->post_author}author" )
-			];
-		} else {
-			$response = array(
-				'message' => 'Recipe API is working!',
-				'request' => $request->get_param( 'slug' )
-			);
-		}
-
-
-		return rest_ensure_response( $response );
-	}
-
-	public function create_a_recipe( $request ) {
-		$response = array(
-			'message' => 'Recipe API is working for post!',
-			'request' => $request->get_body_params()
-		);
-
-		return rest_ensure_response( $response );
+		register_rest_route( $api_name_space, '/recipes/(?P<id>\d+)', array(
+			'methods'  => 'GET, POST, PUT, DELETE',
+			'callback' => array( $this, 'recipe_api_handler' ),
+		) );
 	}
 
 	public function register_user( $request ){
@@ -229,6 +166,101 @@ class Api
 			'user_login' => Helper::prepare_user_login_hash( $user ),
 			'user_caps'  => Helper::prepare_user_caps_hash( $user ),
 		);
+
+		return rest_ensure_response( $response );
+	}
+
+    // Callback function to get all recipes
+	public function get_all_recipes( $request ) {
+		$args = array(
+			'post_type' => 'recipe',
+			'per_page'  => - 1, // Retrieve all posts of the 'recipe' type
+		);
+
+		$query   = new \WP_Query( $args );
+		$recipes = [];
+
+		if ( $query->have_posts() ) {
+			$posts = $query->posts;
+
+			foreach ( $posts as $post ) {
+				$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+				$recipes[] = [
+					'id'          => $post->ID,
+					'title'       => $post->post_title,
+					'description' => $post->post_content,
+					'slug'        => $post->post_name,
+					'image'       => $thumbnail ? $thumbnail[0] : $thumbnail,
+					'ingredients' => get_post_meta( $post->ID, '_recipe_ingredients', true ),
+					'user_hash'   => Helper::encrypt( "{$post->post_author}author" )
+				];
+			}
+		}
+
+		return rest_ensure_response( $recipes );
+	}
+
+	public function get_recipe_by_slug( $request ) {
+
+		$recipe = get_page_by_path( $request->get_param( 'slug' ), OBJECT, 'recipe' );
+		if ( $recipe ) {
+			$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $recipe->ID ), 'full' );
+			$response  = [
+				'id'          => $recipe->ID,
+				'title'       => $recipe->post_title,
+				'description' => $recipe->post_content,
+				'slug'        => $recipe->post_name,
+				'image'       => $thumbnail ? $thumbnail[0] : $thumbnail,
+				'ingredients' => get_post_meta( $recipe->ID, '_recipe_ingredients', true ),
+				'user_hash'   => Helper::encrypt( "{$recipe->post_author}author" )
+			];
+		} else {
+			$response = array(
+				'message' => 'Recipe API is working!',
+				'request' => $request->get_param( 'slug' )
+			);
+		}
+
+
+		return rest_ensure_response( $response );
+	}
+
+	public function create_a_recipe( $request ) {
+
+		$auth_token = $request->get_header('authorization');
+		$auth_data = json_decode( Helper::decrypt($auth_token) );
+
+		if ( !$auth_data ){
+			$response = array(
+				'status'  => 0,
+				'message' => 'Unauthorized Request',
+			);
+			return rest_ensure_response( $response );
+		}
+
+		$user = get_user_by( 'id', $auth_data->id );
+
+		if ( ! user_can( $user->ID, 'edit_posts' ) ) {
+			$response = array(
+				'status'  => 0,
+				'message' => 'User is not capable to create recipe!',
+			);
+
+			return rest_ensure_response( $response );
+		}
+
+		$params = $request->get_json_params();
+
+		if ( empty( $params ) ){
+			$params = $request->get_body_params();
+		}
+
+		$response = array(
+			'message' => 'Recipe API is working for post!',
+			'request' => $params,
+		);
+
+
 
 		return rest_ensure_response( $response );
 	}
